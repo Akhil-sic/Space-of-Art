ec6a032c2a4aa1dad29f54653b8952325b3aceb9
Akhil-sic <akil.varghese@gmail.com>
2021-09-05 09:24:17 +0530

    settings change

--- myproject/settings.py
+++ myproject/settings.py
@@ -151,7 +151,7 @@ EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST= 'smtp.gmail.com'
 EMAIL_PORT= '587'
 EMAIL_USE_TLS= True
-EMAIL_HOST_USER= 'akil.varghese@gmail.com'
-EMAIL_HOST_PASSWORD= 'Wwe1118912'
+EMAIL_HOST_USER= 'your email'
+EMAIL_HOST_PASSWORD= 'Password'
 
 
